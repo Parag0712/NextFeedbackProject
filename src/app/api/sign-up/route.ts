9922568af52befebce236c0ef100003efda81214
parag0712 <paragvadgama123@gmail.com>
2024-07-07 01:01:42 +0530

    singup added

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -3,12 +3,110 @@ import User from "@/model/User";
 import bcrypt from 'bcrypt'
 import { ApiResponse } from '@/types/ApiResponse'
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import { NextResponse } from "next/server";
+
+export interface IUser extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isAcceptingMessage: boolean;
+    isVerified: boolean;
+    messages: IMessage[];
+}
+
+export interface IMessage extends Document {
+    content: string;
+    createdAt: Date;
+}
 
 export async function POST(request: Request) {
     await connect();
 
     try {
-        const {username,email,password} = await request.json();
+        const { username, email, password } = await request.json();
+
+        const exitingUserVerifiedUsername = await User.findOne({ username, isVerified: true })
+        // Check Username exist or not
+        if (exitingUserVerifiedUsername) {
+            return NextResponse.json({
+                success: false,
+                messages: "Username is already taken"
+            },
+                {
+                    status: 400
+                })
+        }
+
+        // Check Email exist or not
+        const exitingUserVerifiedByEmail = await User.findOne({ email })
+        // Generate Verify Code  
+        const verifyCode = Math.floor(10000 + Math.random() * 90000).toString();
+
+        if (exitingUserVerifiedByEmail) {
+
+            if (exitingUserVerifiedByEmail.isVerified) {
+                return NextResponse.json({
+                    success: false,
+                    messages: "Email is already taken"
+                },
+                    {
+                        status: 400
+                    })
+            } else {
+                // If email exit and is not verifed
+                const hasedPassword = await bcrypt.hash(password, 10);
+                exitingUserVerifiedByEmail.password = hasedPassword;
+                exitingUserVerifiedByEmail.verifyCode = verifyCode;
+                exitingUserVerifiedByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000)
+                await exitingUserVerifiedByEmail.save();
+            }
+        } else {
+
+            // Generate Hash password
+            const hasedPassword = await bcrypt.hash(password, 10,)
+
+            // Here exipery date for otp
+            const exipryDate = new Date();
+            exipryDate.setHours(exipryDate.getHours() + 1);
+
+            // Here Save User 
+            const newUser = new User({
+                username,
+                password: hasedPassword,
+                email,
+                verifyCode: verifyCode,
+                isVerified: false,
+                verifyCodeExpiry: exipryDate,
+                isAcceptingMessage: true,
+                messages: [],
+            });
+
+            await newUser.save();
+
+        }
+
+        // Send Email
+        const emailResponse = await sendVerificationEmail(email, username, verifyCode);
+
+        if (!emailResponse.success) {
+            return NextResponse.json({
+                success: false,
+                messages: emailResponse.message
+            }, {
+                status: 500
+            })
+        }
+
+        return NextResponse.json({
+            success: true,
+            messages: "User Register successfully. Please verify your email"
+        }, {
+            status: 201
+        })
+
+
     } catch (error) {
         console.error("Error registering user", error);
         const response: ApiResponse = {
@@ -17,7 +115,7 @@ export async function POST(request: Request) {
         }
         return Response.json(response,
             {
-                status:500
+                status: 500
             }
         )
     }
